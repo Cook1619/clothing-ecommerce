41117d8ed14016703e21fc2b870f814837cc9ca0
Cook1619 <cook6348@gmail.com>
2020-08-25 19:28:55 -0500

    Adding collections and shop page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/Homepage.jsx";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/Shop.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import CollectionPreview from "../../components/collection-preview/Collection-Preview";
+import SHOP_DATA from "./Shop_Data.js";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
