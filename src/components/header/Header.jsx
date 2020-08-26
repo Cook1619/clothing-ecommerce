581bc79e6f26628642f697b82c4e3ed3f5c522b1
Cook1619 <cook6348@gmail.com>
2020-08-26 10:55:43 -0500

    Creating sing in page with sing in page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../firebase/firebase.utils";
+
 // This is a special syntax react has for importing SVGS as a react component
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./Header.scss";
 
-export default function Header() {
-  return (
-    <div className="header">
-      <Link to="/" className="logo-container">
-        <Logo className="logo" />
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
       </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
         </Link>
-        <Link className="option" to="/shop">
-          CONTACT
-        </Link>
-      </div>
+      )}
     </div>
-  );
-}
+  </div>
+);
+// Line 22-27
+// Checks if a user is signed in and with conditionally render the respective element
+// auth.signOut() is method provided by firebase to sign out of our credentials
+export default Header;
